e1907c80ca8d36f4b81d5f8daf654ef445b5a67a
Yannis Burkhalter <yannis@burkhalter.dev>
2024-01-29 14:56:32 +0100

    [FIX] Add missing <ul/>

--- application/views/exploration/category.php
+++ application/views/exploration/category.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <h1><?=$category->cat_name?></h1>
 <a href="<?=base_url('exploration')?>" class="btn btn-outline-primary"><?=$this->lang->line('btn_back')?></a>
-<?php foreach ($series as $serie) { ?>
+<ul><?php foreach ($series as $serie) { ?>
 	<li>
 		<?php if(isset($serie->empty)) { ?>
 			<?=$serie->serie_name?> <?=$this->lang->line('msg_empty')?>
@@ -9,4 +9,4 @@
 			<a href="<?=base_url('exploration/serie/'.$serie->id)?>"><?=$serie->serie_name?></a>
 		<?php } ?>
 	</li>
-<?php } ?>
+<?php } ?></ul>
